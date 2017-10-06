d28f7662d4f280783a42e967141c00badb2af012
Augusto José <ajtgalencar@gmail.com>
2017-10-06 08:48:06 -0300

    Cria subclasses para vazao e para chuva

--- hidroweb/ana_file.py
+++ hidroweb/ana_file.py
@@ -35,27 +35,75 @@ class AnaFile:
                             self.head += line.strip('//') + '\n'
                         if 'Código da Estação' in line:
                             self.station = int(line.strip('\n').split(':')[-1])
-        self.df = self.__get_df()
 
     def __str__(self):
         return self.head
 
-    def __get_df(self):
+    def get_df(self, date_index):
         """ This method reads a csv file as a pandas DataFrame, sets it's index as
         datetime64 and returns the result. """
 
         df = pd.read_csv(self.name + '.zip', header=self.header, sep=';', decimal=',', parse_dates=[2],
                          dayfirst=True)
         df.rename(columns={'//EstacaoCodigo': 'EstacaoCodigo'}, inplace=True)
-        try:
-            df.set_index('Data', inplace=True)
-            del(df['Hora'])
-            df.sort_index(inplace=True)
-        except KeyError:
-            df.set_index('Data', inplace=True)
-            df.sort_index(inplace=True)
-
+        date_index(df)
         return df
+
+
+class AnaFlow(AnaFile):
+
+    def __init__(self, file_name):
+        super().__init__(file_name)
+        self.df = super().get_df(self.date_index)
+
+    @staticmethod
+    def date_index(df):
+        df.set_index('Data', inplace=True)
+        del (df['Hora'])
+        df.sort_index(inplace=True)
+
+    def save_df(self):
+        options = ('JSON', 'CSV')
+        for i, option in enumerate(options):
+            print('%s - %s' % (i, option))
+        option = int(input('\nChoose an option: '))
+
+        if option == 0:
+            self.df.to_json(self.name.lower() + str(self.station) + '.json', date_format='iso')
+        elif option == 1:
+            self.df.to_csv(self.name.lower() + str(self.station) + '.csv')
+        else:
+            print('Invalid option. Try again.')
+
+    def plot_line(self):
+        """This method plots a line graph of the DataFrame"""
+
+        # Plots 'NivelConsistencia' 1 and 2:
+
+        trace0 = go.Scatter(x=self.df.index[self.df.NivelConsistencia == 1],
+                            y=self.df.Maxima[self.df.NivelConsistencia == 1],
+                            name="Raw")
+        trace1 = go.Scatter(x=self.df.index[self.df.NivelConsistencia == 2],
+                            y=self.df.Maxima[self.df.NivelConsistencia == 2],
+                            name="Consistent")
+        data = [trace0, trace1]
+        layout = dict(title='Station ' + str(self.station),
+                      xaxis=dict(title='Date'),
+                      yaxis=dict(title='Maximum Flow (m³/s)'),
+                      )
+        plotly.offline.plot({'data': data, 'layout': layout})
+
+
+class AnaRain(AnaFile):
+
+    def __init__(self, file_name):
+        super().__init__(file_name)
+        self.df = super().get_df(self.date_index)
+
+    @staticmethod
+    def date_index(df):
+        df.set_index('Data', inplace=True)
+        df.sort_index(inplace=True)
 
     def save_df(self):
         options = ('JSON', 'CSV')

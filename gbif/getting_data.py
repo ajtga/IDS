2db9f6d46075fbf64ec2a2f2bb92c805f1abba37
Augusto José <ajtgalencar@gmail.com>
2017-10-16 20:04:13 -0300

    Atualiza exemplo de uso da API do GBIF

--- gbif/getting_data.py
+++ gbif/getting_data.py
@@ -5,7 +5,7 @@ api_base = 'http://api.gbif.org/v1/'
 complement = 'occurrence/search?year=1917,2017'
 request = requests.get(api_base + complement)
 
+print(request.json().keys())
 results = request.json()['results']
-request.json().keys()
 
 df = pd.DataFrame(results)

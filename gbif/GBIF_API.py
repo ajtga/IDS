83183fdc6626a6947c933601a6cc9fd75a7317d7
Augusto José <ajtgalencar@gmail.com>
2017-10-28 16:13:18 -0300

    Cria função para obter dados de ocorrencia

---
+++ gbif/GBIF_API.py
@@ -0,0 +1,27 @@
+import requests
+import pandas as pd
+
+
+def dados_ocorrencia(ano, quantidade=300):
+    api = 'http://api.gbif.org/v1/'
+    lista_dfs = []
+    offset = 0
+    iteracoes = int(quantidade/300)
+    resto = quantidade % 300
+    for i in range(iteracoes):
+        ocorrencia = ('occurrence/search?year={}&limit=300&offset={}'.format(ano, offset))
+        request = requests.get(api + ocorrencia)
+        try:
+            lista_dfs.append(pd.DataFrame(request.json()['results']))
+        except:
+            print('Erro')
+        finally:
+            offset += 300
+    ocorrencia = 'occurrence/search?year={}&limit={}&offset={}'.format(ano, resto, offset)
+    request = requests.get(api + ocorrencia)
+    try:
+        lista_dfs.append(pd.DataFrame(request.json()['results']))
+    except:
+        print('Erro no ultimo')
+    finally:
+        return pd.concat(lista_dfs)

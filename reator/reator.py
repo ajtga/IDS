6b2f4b5c113e60d8a8af1619fc97a6d665bf0afd
Adelson Jr <adelson.ss.jr@hotmail.com>
2017-12-05 10:09:53 -0300

    Dados relacionados

--- reator/reator.py
+++ reator/reator.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import xlrd
+import plotly
+import plotly.graph_objs as go
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
@@ -46,14 +48,18 @@ class Reator:
         planilha = int(input("Numero da Planilha: "))-1
         df = self.get_df(planilha)
 
+
         app = dash.Dash()
 
         lista_col = []
-        for i in df.columns:
+        print("\n\nSelecione a variavel que sera relacionada:")
+        for num, i in enumerate(df.columns):
+            print("%d - %s"%(num, i))
             if "Tempo" not in i:
                 if "dados" not in i:
                     lista_col.append({'label': i, 'value': i})
-
+        relacao = int(input("Digite o numero referente:"))
+        relacao = df.columns[relacao]
         app.layout = html.Div(
         children=[html.H1(children='Graficos da planilha',
         style={'textAlign': 'center'}),
@@ -62,17 +68,17 @@ class Reator:
         dcc.Dropdown(
             id='my-dropdown',
             options=lista_col,
-            value='TDH - TDH'
+            value='Concentração de Glicose (mg/L) - Afluente'
             ),
         dcc.Graph(id='my-graph')
         ])
 
         @app.callback(Output('my-graph', 'figure'), [Input('my-dropdown', 'value')])
         def update_graph(selected_dropdown_value):
-            df = self.dataframe
+            df = self.dataframe.sort_values(by=relacao)
             return {
                 'data': [{
-                'x': df['Tempo - Data'],
+                'x': df[relacao],
                 'y': df[selected_dropdown_value]
                 }]
                 }
@@ -126,3 +132,65 @@ class Reator:
             'data': data
         }
         app.run_server()
+
+    def graf_uni(self):
+        """Multiple Y-Axes"""
+        print('Menu das tabelas:')
+        lista_planilha = ['R2CA', 'R2SA', 'R4CA', 'R4SA', 'R10CA', 'R10SA',
+        'R25CA', 'R25SA']
+        for i in range(8):
+            print('%i - %s' % (i+1, lista_planilha[i]))
+        planilha = int(input("Numero da Planilha: "))-1
+        df = self.get_df(planilha)
+
+        relacao = [3, 5, 7, 9, 16, 20]
+
+        data = []
+        cont = 2
+        flag = True
+        cor = ['black', 'red', 'green', 'purple', 'blue', 'brown']
+        for num, i in enumerate(df.columns):
+            if num in relacao:
+                if flag:
+                    data.append(go.Scatter(
+                    x=df['Tempo - Dia'], y=df[i], name=i,
+                    line=dict(color=cor[0])
+                    ))
+                    flag = False
+                else:
+                    if i == 'Vol H2 (L) - VH':
+                        data.append(go.Scatter(
+                        x=df['Tempo - Dia'], y=df[i], name=i,
+                        line=dict(color=cor[cont-1], width=4),
+                        yaxis=('y'+'%d'%(cont))
+                        ))
+                        cont += 1
+                    else:
+                        data.append(go.Scatter(
+                        x=df['Tempo - Dia'], y=df[i], name=i,
+                        line=dict(color=cor[cont-1]), yaxis=('y'+'%d'%(cont))
+                        ))
+                        cont += 1
+
+        layout = go.Layout(
+        title="Grafico relacionado", xaxis=dict(domain=[0.2, 0.8]),
+        yaxis=dict(title='Concentração Glicose', titlefont=dict(color='black'),
+        tickfont=dict(color='black')),
+        yaxis2=dict(title='DQO', titlefont=dict(color='red'),
+        tickfont=dict(color='red'), overlaying='y', anchor='free', side='left',
+        position=0),
+        yaxis3=dict(title='pH', titlefont=dict(color='green'),
+        tickfont=dict(color='green'), overlaying='y', anchor='free',
+        side='right', position=0.9),
+        yaxis4=dict(title='Acidos Volateis', titlefont=dict(color='purple'),
+        tickfont=dict(color='purple'), overlaying='y', anchor='free',
+        side='left', position=0.1),
+        yaxis5=dict(title='Vol H2', titlefont=dict(color='blue'),
+        tickfont=dict(color='blue'), overlaying='y', anchor='free',
+        side='right', position=0.8),
+        yaxis6=dict(title='mol H2/mol Glicose', titlefont=dict(color='brown'),
+        tickfont=dict(color='brown'), overlaying='y', anchor='free',
+        side='right', position=1)
+        )
+        fig = go.Figure(data=data, layout=layout)
+        plotly.offline.plot(fig)
